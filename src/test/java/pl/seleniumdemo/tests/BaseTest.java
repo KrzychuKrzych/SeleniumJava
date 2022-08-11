c590761e53888cfeed2cb49e3050b7c78250c8d6
Krzysztof <kszyneg@gmail.com>
2022-08-11 20:00:05 +0200

    Add logs

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pl.seleniumdemo.utils.DriverFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,8 +14,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = DriverFactory.getDriver("chrome");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
